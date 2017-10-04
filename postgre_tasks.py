00c31f7cba32fda6d267de87dfd6f97a98fa36fa
xofbd <dfox09@gmail.com>
2017-10-04 09:02:02 -0400

    fetch username

--- postgre_tasks.py
+++ postgre_tasks.py
@@ -1,3 +1,4 @@
+import os
 import psycopg2
 
 
@@ -5,40 +6,46 @@ class PostgreTasks(object):
 
     def __init__(self, csv_file):
         self.csv_file = csv_file
-        self.conn = psycopg2.connect(dbname="dbf", user="dbf")
-        self.cur = self.conn.cursor()
 
-        self.cur.execute("DROP TABLE IF EXISTS test_table_2;")
+        # get user name for postgre connection
+        user = os.popen("echo $USER").read().strip()
+        dbname = user
+
+        # create psql connection, cursor, and create test table
+        self.conn = psycopg2.connect(dbname=dbname, user=user)
+        self.cur = self.conn.cursor()
+        self.cur.execute("DROP TABLE IF EXISTS test_table;")
 
         query = """
-        CREATE TABLE test_table_2
+        CREATE TABLE test_table
         (score_1 float, score_2 float, section char(1));
         """
+
         self.cur.execute(query)
 
     def load(self):
-        self.cur.execute("DELETE FROM test_table_2;")
+        self.cur.execute("DELETE FROM test_table;")
 
         with open(self.csv_file, 'r') as f:
-            self.cur.copy_from(f, "test_table_2", sep=',')
+            self.cur.copy_from(f, "test_table", sep=',')
 
     def select(self):
-        self.cur.execute('SELECT section FROM test_table_2;')
+        self.cur.execute('SELECT section FROM test_table;')
 
     def filter(self):
         self.cur.execute(
-            "SELECT section FROM test_table_2 WHERE section = 'A';")
+            "SELECT section FROM test_table WHERE section = 'A';")
 
     def groupby_agg(self):
         query = """
         SELECT AVG(score_1)
-        FROM test_table_2
+        FROM test_table
         GROUP BY section;
         """
 
         self.cur.execute(query)
 
     def get_num_rows(self):
-        self.cur.execute("SELECT COUNT(*) FROM test_table_2;")
+        self.cur.execute("SELECT COUNT(*) FROM test_table;")
         num_rows = self.cur.fetchall()
         return int(num_rows[0][0])
