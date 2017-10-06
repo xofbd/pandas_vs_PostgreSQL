19b2bb894b464e3a247cb28b20a58e50735df172
xofbd <dfox09@gmail.com>
2017-10-06 15:44:44 -0400

    make more general

---
+++ create_jsons.py
@@ -0,0 +1,36 @@
+import json
+
+
+def create_json(results_file):
+    tasks = ('load', 'select_', 'filter', 'groupby_agg')
+    results_dict = dict(zip(tasks, range(len(tasks))))
+
+    with open(results_file, 'r') as f:
+        task = f.readline()
+
+        replicates = []
+        results = f.read().split()
+
+        for r in results:
+            if r in results_dict.keys():
+                results_dict[task] = results
+                task = r
+                replicates = []
+            else:
+                replicates.append(int(r))
+
+    # dump dictionary to json
+    with open('results.json', 'w') as f:
+        json.dump(results_dict, f)
+
+
+def join_jsons(json_list, dict_keys):
+    new_json = {}
+    for f, key in zip(json_list, dict_keys):
+        new_json[key] = json.load(f)
+
+if __name__ == '__main__':
+    import sys
+
+    for
+    create_json(sys.argv[1])
