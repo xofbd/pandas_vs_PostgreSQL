bec523ce0784a9a97bee1814a30f889af80631f3
xofbd <dfox09@gmail.com>
2017-10-15 21:10:13 -0400

    add missing column in csv B

--- create_dataset.py
+++ create_dataset.py
@@ -16,9 +16,8 @@ def create_csv(n=1000):
     score_3 = np.random.rand(n)
 
     # create and dump DataFrame to csv
-    df_A = pd.DataFrame(dict(zip(columns, [id, section, score_1, score_2])))
-    df_B = pd.DataFrame({columns[0]: score_3})
-
+    df_A = pd.DataFrame(dict(zip(columns, (id, section, score_1, score_2))))
+    df_B = pd.DataFrame(dict(zip(('id', 'score_3'), (id, score_3))))
     df_A.to_csv('csv/A/test_A_' + str(n) +
                 '_rows.csv', index=False, header=False)
     df_B.to_csv('csv/B/test_B_' + str(n) +
