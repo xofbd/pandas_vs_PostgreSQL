2175bfc9f3e418418dfade6a47e6fd4bfd875a68
xofbd <dfox09@gmail.com>
2017-10-27 17:40:25 -0400

    bechmark load

--- pgbench_queries.sh
+++ pgbench_queries.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
+#
+# 2.) combine log results (python script to do this)
+# 2.a) place all log files of a certain row number into separate directories
+# 2.b) loop through all directories, creating json
 
-# initialize tables
+
+# initialize tables and directories
 psql -U $USER -d $USER --file=queries/init.sql > /dev/null
 
-for file_A in csv/A/test_A_1000000_rows.csv; do  #csv/A/*; do
+if [ ! -d log ]; then
+    mkdir log
+fi
+
+for file_A in csv/A/*; do
     num_rows=$(echo $file_A | grep -oP '\d+')
     file_B="csv/B/test_B_"$num_rows"_rows.csv"
     echo $file_A
+
+    echo "DELETE FROM test_table_A;" > queries/load.sql
+    echo "DELETE FROM test_table_B;" > queries/load.sql
     
-    query_A="\COPY test_table_A FROM "$file_A" WITH DELIMITER ',';"
-    query_B="\COPY test_table_B FROM "$file_B" WITH DELIMITER ',';"
+    query_A="COPY test_table_A FROM ""'""$PWD"/"$file_A""'"" WITH DELIMITER ',';"
+    query_B="COPY test_table_B FROM ""'""$PWD"/"$file_B""'"" WITH DELIMITER ',';"
     
-    psql -U $USER -d $USER -c "$query_A" > /dev/null
-    psql -U $USER -d $USER -c "$query_B" > /dev/null
-    #pgbench -U $USER -d $USER -c "$query_A" > /dev/null
-    #pgbench -U $USER -d $USER -c "$query_B" > /dev/null
+    echo "$query_A" >> queries/load.sql
+    echo "$query_B" >> queries/load.sql
+    
+    pgbench -ln -t $1 --file=queries/load.sql > /dev/null
+    mv pgbench_log* log/pgbench_load_$num_rows".log"
+    # pgbench -ln -t $1 --file=queries/load_B.sql > /dev/null
+    # mv pgbench_log* log/pgbench_load_B_$num_rows".log"
     
     for task in select filter groupby_agg join; do
 	echo $task
-	pgbench -l -n -t $1 -U $USER --file=queries/$task.sql > /dev/null
-	mv pgbench_log* pgbench_$task"_"$num_rows".log"
+	pgbench -ln -t $1 --file=queries/$task.sql > /dev/null
+	mv pgbench_log* log/pgbench_$task"_"$num_rows".log"
     done
 done
 
+# format results from logs
+#if [ -d log ]; then
+#    rm -rf log 
+#fi	    
+		
 # remove created tables
 psql -U $USER -d $USER --file=queries/clean_up.sql
