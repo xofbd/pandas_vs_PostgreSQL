77ac062aad32152c2fb360f293e40740ab5d6a98
xofbd <dfox09@gmail.com>
2017-10-29 18:23:34 -0400

    add comments

--- pgbench_queries.sh
+++ pgbench_queries.sh
@@ -1,51 +1,50 @@
 #!/bin/bash
 #
-# 2.) combine log results (python script to do this)
-# 2.a) place all log files of a certain row number into separate directories
-# 2.b) loop through all directories, creating json
+# Run PostgreSQL benchmark using pgbench. The only positional parameter is the
+# the number of benchmark replicates.
 
-
-# initialize tables and directories
-#psql -U $USER -d $USER --file=queries/init.sql > /dev/null
-
+# initialize directories
 if [ ! -d log ]; then
     mkdir log
 fi
 
 for file_A in csv/A/*; do
+    # initialize table and variables
     psql -U $USER -d $USER --file=queries/init.sql > /dev/null
     num_rows=$(echo $file_A | grep -oP '\d+')
+    echo $num_rows" rows"
+
+    # create and run loading csv query file
     file_B="csv/B/test_B_"$num_rows"_rows.csv"
-    echo $file_A
+    query_A="COPY test_table_A FROM ""'""$PWD"/"$file_A""'"" WITH DELIMITER ',';"
+    query_B="COPY test_table_B FROM ""'""$PWD"/"$file_B""'"" WITH DELIMITER ',';"
 
     echo "DELETE FROM test_table_A;" > queries/load.sql
     echo "DELETE FROM test_table_B;" >> queries/load.sql
-    
-    query_A="COPY test_table_A FROM ""'""$PWD"/"$file_A""'"" WITH DELIMITER ',';"
-    query_B="COPY test_table_B FROM ""'""$PWD"/"$file_B""'"" WITH DELIMITER ',';"
-    
     echo "$query_A" >> queries/load.sql
     echo "$query_B" >> queries/load.sql
-    
-    # pgbench -ln -t 1 --file=queries/load.sql > /dev/null
-    # mv pgbench_log* log/pgbench_load_$num_rows".log"
+
     psql -U $USER -d $USER --file=queries/load.sql > /dev/null
+
+    # benchmark each task
     for task in select filter groupby_agg join; do
 	echo $task
 	pgbench -ln -t $1 --file=queries/$task.sql > /dev/null
 	mv pgbench_log* log/pgbench_$task"_"$num_rows".log"
     done
-    pgbench -ln -t $1 --file=queries/load.sql > /dev/null
+
+    # benchmark loading csv files
+    echo "DELETE FROM test_table_A;" > queries/load_A.sql
+    echo "$query_A" >> queries/load_A.sql
+    
+    pgbench -ln -t $1 --file=queries/load_A.sql > /dev/null
     mv pgbench_log* log/pgbench_load_$num_rows".log"
     psql -U $USER -d $USER --file=queries/clean_up.sql
 done
 
+# format results from logs and clean up		
 python create_json.py
 
-# format results from logs
 if [ -d log ]; then
     rm -rf log 
-fi	    
-		
-# remove created tables
-#psql -U $USER -d $USER --file=queries/clean_up.sql
+fi
