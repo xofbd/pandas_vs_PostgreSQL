ad4d6f17a558115f6741a60c084debc4942f00eb
xofbd <dfox09@gmail.com>
2017-10-29 00:06:57 -0400

    run creat_json.py

--- pgbench_queries.sh
+++ pgbench_queries.sh
@@ -6,19 +6,20 @@
 
 
 # initialize tables and directories
-psql -U $USER -d $USER --file=queries/init.sql > /dev/null
+#psql -U $USER -d $USER --file=queries/init.sql > /dev/null
 
 if [ ! -d log ]; then
     mkdir log
 fi
 
 for file_A in csv/A/*; do
+    psql -U $USER -d $USER --file=queries/init.sql > /dev/null
     num_rows=$(echo $file_A | grep -oP '\d+')
     file_B="csv/B/test_B_"$num_rows"_rows.csv"
     echo $file_A
 
     echo "DELETE FROM test_table_A;" > queries/load.sql
-    echo "DELETE FROM test_table_B;" > queries/load.sql
+    echo "DELETE FROM test_table_B;" >> queries/load.sql
     
     query_A="COPY test_table_A FROM ""'""$PWD"/"$file_A""'"" WITH DELIMITER ',';"
     query_B="COPY test_table_B FROM ""'""$PWD"/"$file_B""'"" WITH DELIMITER ',';"
@@ -26,20 +27,25 @@ for file_A in csv/A/*; do
     echo "$query_A" >> queries/load.sql
     echo "$query_B" >> queries/load.sql
     
-    pgbench -ln -t $1 --file=queries/load.sql > /dev/null
-    mv pgbench_log* log/pgbench_load_$num_rows".log"
-    
+    # pgbench -ln -t 1 --file=queries/load.sql > /dev/null
+    # mv pgbench_log* log/pgbench_load_$num_rows".log"
+    psql -U $USER -d $USER --file=queries/load.sql > /dev/null
     for task in select filter groupby_agg join; do
 	echo $task
 	pgbench -ln -t $1 --file=queries/$task.sql > /dev/null
 	mv pgbench_log* log/pgbench_$task"_"$num_rows".log"
     done
+    pgbench -ln -t $1 --file=queries/load.sql > /dev/null
+    mv pgbench_log* log/pgbench_load_$num_rows".log"
+    psql -U $USER -d $USER --file=queries/clean_up.sql
 done
 
+python create_json.py
+
 # format results from logs
-#if [ -d log ]; then
-#    rm -rf log 
-#fi	    
+if [ -d log ]; then
+    rm -rf log 
+fi	    
 		
 # remove created tables
-psql -U $USER -d $USER --file=queries/clean_up.sql
+#psql -U $USER -d $USER --file=queries/clean_up.sql
